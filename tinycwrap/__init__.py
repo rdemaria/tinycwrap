26dc3a3619a59067eb90dbb85b7895f6f74895c1
Riccardo De Maria <riccardodemaria@gmail.com>
2025-11-26 15:13:41 +0100

    Release v0.0.3

--- tinycwrap/__init__.py
+++ tinycwrap/__init__.py
@@ -6,6 +6,6 @@ from .cmodule import CModule
 try:
     __version__ = version("tinycwrap")
 except PackageNotFoundError:
-    __version__ = "0.0.2"
+    __version__ = "0.0.3"
 
 __all__ = ["CModule", "__version__"]
